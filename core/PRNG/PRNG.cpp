2d49799d30700d07726879b4d68e8cd1017343a7
Santinorua <santinoruani@gmail.com>
2026-03-22 21:19:31 -0300

    PRNG Redone

--- core/PRNG/PRNG.cpp
+++ core/PRNG/PRNG.cpp
@@ -1,26 +1,25 @@
 #include "PRNG.h"
 
 namespace WorldMaker {
-    uint64_t PRNG::nextNumber64() {
+    uint64_t PRNG::nextNumber64(uint64_t seed) {
         // Xor Shift*
         uint64_t x = seed;
         x ^= x >> 12;
         x ^= x << 25;
         x ^= x >> 27;
-        seed = x * 0x2545F4914F6CDD1DULL;
-        return seed;
+        return x * 0x2545F4914F6CDD1DULL;
     }
 
-    uint32_t PRNG::nextNumber32() {
+    uint32_t PRNG::nextNumber32(uint64_t seed) {
         // Convert the 64-bit number to a 32-bit number by taking the lower 32 bits
-        return static_cast<uint32_t>(nextNumber64() & 0xFFFFFFFF);
+        return static_cast<uint32_t>(nextNumber64(seed) & 0xFFFFFFFF);
     }
 
-    std::vector<uint64_t> PRNG::nextMultipleNumbers(int count) {
-        std::vector<uint64_t> numbers;
-        for (int i = 0; i < count; i++) {
-            numbers.push_back(nextNumber64());
-        }
-        return numbers;
+    Vec2 PRNG::randomVector2(int x, int y, uint64_t seed) {
+        uint64_t newSeed = static_cast<uint64_t>(nextNumber32(seed)) * static_cast<uint64_t>(x) * static_cast<uint64_t>(y);
+        uint64_t r = nextNumber32(newSeed);
+        double x = static_cast<double>(r % 200001) / 100000.0 - 1.0;
+        double y = static_cast<double>(nextNumber32(r) % 200001) / 100000.0 - 1.0;
+        return Vec2(x, y);
     }
 }
--- core/PRNG/PRNG.h
+++ core/PRNG/PRNG.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include "Vec2.h"
+
 
 namespace WorldMaker {
     struct PRNG {
-        uint64_t seed;
-        uint64_t nextNumber64();
-        uint32_t nextNumber32();
-        std::vector<uint64_t> nextMultipleNumbers(int count);
+        uint64_t nextNumber64(uint64_t seed);
+        uint32_t nextNumber32(uint64_t seed);
+        Vec2 randomVector2(int x, int y, seed);
     };
 }
