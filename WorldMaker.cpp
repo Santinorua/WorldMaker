b7d67135e06a33d213f53191860b01da500e2555
coolfede97 <coolfede97@gmail.com>
2026-06-02 10:15:33 -0300

    changes of shaderProgramByType and start of fix for noiseRenderUnit before showing to jero

--- WorldMaker.cpp
+++ WorldMaker.cpp
@@ -37,42 +37,57 @@ int main()
 	int gridWidth = ChunkRenderUnit::chunkWidth;
     int gridDepth = ChunkRenderUnit::chunkHeight;
 
-    std::vector<Vertex> chunkVertices;
-    chunkVertices.reserve(gridWidth * gridDepth);
+    // std::vector<Vertex> chunkVertices;
+    // chunkVertices.reserve(gridWidth * gridDepth);
 
-    FractalNoise fractal(gridWidth, gridDepth, 5, 2, 1, 4, 2.0, 0.75);
-    for (int z = 0; z < gridDepth; ++z)
+    // FractalNoise fractal(gridWidth, gridDepth, 5, 2, 1, 4, 2.0, 0.75);
+    // for (int z = 0; z < gridDepth; ++z)
+    // {
+    //     for (int x = 0; x < gridWidth; ++x)
+    //     {
+
+    //         double posY = fractal.getNoise(x, z);
+
+    //         double hL = fractal.getNoise(x - 1, z);
+    //         double hR = fractal.getNoise(x + 1, z);
+    //         double hD = fractal.getNoise(x, z - 1);
+    //         double hU = fractal.getNoise(x, z + 1);
+
+    //         glm::vec3 normal;
+    //         normal.x = static_cast<float>(hL - hR);
+    //         normal.y = static_cast<float>(2.0 * 1);
+    //         normal.z = static_cast<float>(hD - hU);
+
+    //         normal = glm::normalize(normal);
+
+    //         Vertex v;
+    //         v.m_color = {1,1,1,1};
+    //         v.m_uv = { static_cast<float>(x) / 10.0f, static_cast<float>(z) / 10.0f };
+    //         v.m_position = { static_cast<float>(x), static_cast<float>(posY), static_cast<float>(z) };
+    //         v.m_normal = normal;
+
+    //         chunkVertices.push_back(v);
+    //     }
+    // }
+
+    // ChunkRenderUnit chunk{chunkVertices};
+
+    // Renderer::PrepareToDrawChunk(chunk);
+
+    std::vector<double> pixels;
+    int width = 10;
+    int height = 10;
+    for (int x = 0; x < width; x++)
     {
-        for (int x = 0; x < gridWidth; ++x)
+        for (int y = 0; y < height; y++)
         {
-
-            double posY = fractal.getNoise(x, z);
-
-            double hL = fractal.getNoise(x - 1, z);
-            double hR = fractal.getNoise(x + 1, z);
-            double hD = fractal.getNoise(x, z - 1);
-            double hU = fractal.getNoise(x, z + 1);
-
-            glm::vec3 normal;
-            normal.x = static_cast<float>(hL - hR);
-            normal.y = static_cast<float>(2.0 * 1);
-            normal.z = static_cast<float>(hD - hU);
-
-            normal = glm::normalize(normal);
-
-            Vertex v;
-            v.m_color = {1,1,1,1};
-            v.m_uv = { static_cast<float>(x) / 10.0f, static_cast<float>(z) / 10.0f };
-            v.m_position = { static_cast<float>(x), static_cast<float>(posY), static_cast<float>(z) };
-            v.m_normal = normal;
-
-            chunkVertices.push_back(v);
+            pixels.push_back(1);
+            pixels.push_back(1);
+            pixels.push_back(1);
+            pixels.push_back(1);
         }
     }
-
-    ChunkRenderUnit chunk{chunkVertices};
-
-    Renderer::PrepareToDrawChunk(chunk);
+    NoiseRenderUnit noise{width, height, pixels};
 
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
@@ -80,6 +95,7 @@ int main()
     ImGui_ImplOpenGL3_Init();
     ImGui::StyleColorsDark();
 
+    Renderer::PrepareToDrawNoise(noise);
 	while (!Renderer::WindowShouldClose())
 	{
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -98,7 +114,8 @@ int main()
         ShaderProgram::s_boundShader->updateCameraMatrices();
         GlobalLight::LoadLightSettings();
 
-        Renderer::DrawChunk(chunk);
+        Renderer::DrawNoise(noise);
+        // Renderer::DrawChunk(chunk);
 		ImGui::Render();
        	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(Renderer::GetWindow());
