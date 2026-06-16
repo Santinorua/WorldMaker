78195e2213baf11b01be6a6025e57aa08ab23323
Santinorua <santinoruani@gmail.com>
2026-06-16 09:46:26 -0300

    Algun tipo de terreno

--- core/worldGenerator/CoreGenerator.cpp
+++ core/worldGenerator/CoreGenerator.cpp
@@ -17,17 +17,17 @@ namespace WorldMaker {
         nextSeed = PRNG::nextNumber64(nextSeed);
         m_humidity = FractalNoise(20.0, 1.0, nextSeed, 4, 2.5, 0.25);
         nextSeed = PRNG::nextNumber64(nextSeed);
-        m_base = FractalNoise(100.0, 1.5, nextSeed, 4, 2.0, 0.5);
+        m_base = FractalNoise(60.0, 1.5, nextSeed, 4, 2.0, 0.5);
     }
 
     double WorldGenerator::getHeight(float x, float z) {
         double continentalness = m_continentalness.getNoise(x, z);
-        double erosion = m_erosion.getNoise(x, z) + 0.5;
+        double erosion = (m_erosion.getNoise(x, z) + 0.5) * 0.5;
         double base = m_base.getNoise(x, z);
         double continentalnessModifier;
 
         if (continentalness >= -0.1) {
-            continentalnessModifier = std::max(0.0, continentalness);
+            continentalnessModifier = 0;
 
         } else if (continentalness >= -0.6) {
             continentalnessModifier = Lerp(-0.8, 0.0, ((continentalness+0.6) * 2), true);
@@ -35,7 +35,7 @@ namespace WorldMaker {
             continentalnessModifier = -0.8;
         }
 
-        double final = base * erosion + continentalnessModifier * erosion;
+        double final = base * erosion + continentalnessModifier;
 
         return final * m_yScale;
     }
