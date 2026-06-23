6665789a2b7a16aee1a2ff3221d3e53b6270dcd9
Coolfede97 <coolfede97@gmail.com>
2026-06-23 16:30:40 -0300

    forgot to remove the original resizeSSBO

--- core/rendering/ChunkRenderUnit.h
+++ core/rendering/ChunkRenderUnit.h
@@ -32,20 +32,5 @@ namespace WorldMaker
 		SSBOUPtr<Vertex> m_vertices = std::make_unique<SSBO<Vertex>>(maxVertexCount, GL_DYNAMIC_STORAGE_BIT);
 		SSBOUPtr<unsigned int> m_indices = std::make_unique<SSBO<unsigned int>>(maxIndexCount, GL_DYNAMIC_STORAGE_BIT);
 
-		template<typename T>
-        void resizeSSBO(SSBOUPtr<T>& ssbo, bool batchExceedsCapacity, unsigned int elementsToSupport)
-        {
-            unsigned int amplifier = batchExceedsCapacity ? ssboOversizeMultiplier : ssboAmplifier;
-			SSBOUPtr<T> newSSBO = std::make_unique<SSBO<T>>((ssbo->maxElements())*amplifier, GL_DYNAMIC_STORAGE_BIT);
-			newSSBO.get()->m_data = ssbo.get()->m_data;
-			newSSBO.get()->m_currentBytes = ssbo.get()->m_currentBytes;
-
-			GLCall(glBindBuffer(GL_COPY_WRITE_BUFFER, newSSBO.get()->glName()));
-			GLCall(glBindBuffer(GL_COPY_READ_BUFFER, ssbo.get()->glName()));
-
-			GLCall(glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, 0, 0, ssbo->maxSize()));
-			ssbo = std::move(newSSBO);
-			return;
-		}
 	};
 }
