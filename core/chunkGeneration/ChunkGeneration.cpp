b9768e555f5ffb3372373b93125e0ec20d5e74a6
Tomás Atanasoff <tomyatana@gmail.com>
2026-07-05 20:57:20 -0300

    changed signature

--- core/chunkGeneration/ChunkGeneration.cpp
+++ core/chunkGeneration/ChunkGeneration.cpp
@@ -9,43 +9,6 @@ ChunkRenderUnit* GenerateChunk(FractalNoise& fractal, float x_offset, float z_of
 	int gridWidth = ChunkRenderUnit::chunkWidth;
     int gridDepth = ChunkRenderUnit::chunkHeight;
 
-    // std::vector<Vertex> chunkVertices;
-    // chunkVertices.reserve(gridWidth * gridDepth);
-
-    // FractalNoise fractal(gridWidth, gridDepth, 5, 2, 1, 4, 2.0, 0.75);
-    // for (int z = 0; z < gridDepth; ++z)
-    // {
-    //     for (int x = 0; x < gridWidth; ++x)
-    //     {
-
-    //         double posY = fractal.getNoise(x, z);
-
-    //         double hL = fractal.getNoise(x - 1, z);
-    //         double hR = fractal.getNoise(x + 1, z);
-    //         double hD = fractal.getNoise(x, z - 1);
-    //         double hU = fractal.getNoise(x, z + 1);
-
-    //         glm::vec3 normal;
-    //         normal.x = static_cast<float>(hL - hR);
-    //         normal.y = static_cast<float>(2.0 * 1);
-    //         normal.z = static_cast<float>(hD - hU);
-
-    //         normal = glm::normalize(normal);
-
-    //         Vertex v;
-    //         v.m_color = {1,1,1,1};
-    //         v.m_uv = { static_cast<float>(x) / 10.0f, static_cast<float>(z) / 10.0f };
-    //         v.m_position = { static_cast<float>(x), static_cast<float>(posY), static_cast<float>(z) };
-    //         v.m_normal = normal;
-
-    //         chunkVertices.push_back(v);
-    //     }
-    // }
-
-    // ChunkRenderUnit chunk{chunkVertices};
-
-    // Renderer::PrepareToDrawChunk(chunk);
-
     std::vector<double> pixels;
     int width = 10;
     int height = 10;
@@ -103,15 +66,15 @@ ChunkRenderUnit* GenerateChunk(FractalNoise& fractal, float x_offset, float z_of
     return new ChunkRenderUnit{chunkVertices};
 }
 
-void RegenerateChunks(std::vector<ChunkRenderUnit*>& chunks, FractalNoise& fractal, int width, int height)
+void RegenerateChunks(std::vector<ChunkRenderUnit*>& chunks, FractalNoise& fractal, int render_distance, glm::vec3 pos)
 {
 	for (ChunkRenderUnit *ck : chunks) {
 		delete ck;
 	}
 	chunks.clear();
 
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
+	for (int x = 0; x < render_distance; x++) {
+		for (int y = 0; y < render_distance; y++) {
 			chunks.push_back(GenerateChunk(fractal, x, y));
 		}
 	}
