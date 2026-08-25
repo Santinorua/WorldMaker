ce1226c84817c54df1d989244abaecf1d271fb27
Federico Ugarte <coolfede97@gmail.com>
2026-08-25 10:39:25 -0300

    in the middle of organizing folders

---
+++ core/rendering/WorldWater.cpp
@@ -0,0 +1,6 @@
+#include "WorldWater.h"
+
+namespace WorldMaker
+{
+    float WorldWater::yPos = 0.0f;
+}
---
+++ core/rendering/WorldWater.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace WorldMaker
+{
+    struct WorldWater
+    {
+        static float yPos;
+    };
+}
