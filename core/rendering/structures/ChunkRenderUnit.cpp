25433f25758cff6c0ccfbe63fc7c3a0be6ad7d1f
Santinorua <santinoruani@gmail.com>
2026-09-22 09:56:02 -0300

    Eliminar prints

--- core/rendering/structures/ChunkRenderUnit.cpp
+++ core/rendering/structures/ChunkRenderUnit.cpp
@@ -43,9 +43,6 @@ namespace WorldMaker
 
 		m_tallestPoint = tallest_point;
 		m_lowestPoint = lowest_point;
-
-		std::cout << "New tallest for " << this << ": " << tallest_point << '\n';
-		std::cout << "New lowest for " << this << ": " << lowest_point << '\n';
 	}
 
 	ChunkRenderUnit::ChunkRenderUnit(std::vector<Vertex>& vertices, double tallestPoint, double lowestPoint, ChunkModels chunkModels, int lod)
